384902bbe872a930485d44374f610c479cbdbc6f
PerezChris99 <kweeziperez712@gmail.com>
2023-08-04 14:39:03 +0300

    variables and constants

--- 02_variables.php
+++ 02_variables.php
@@ -1,13 +1,46 @@
 <?php
 /*----php data types---*/
 /* 
-string series of characters surrounded by quotes
-integer whole numbers
-float decimal numbers
-boolean true or false
-array special variable, which can hold more than one element or value
-object a class
-NULL emplty variable
-Resource special variable that holds a resource
+-string series of characters surrounded by quotes
+-integer whole numbers
+-float decimal numbers
+-boolean true or false
+-array special variable, which can hold more than one element or value
+-object a class
+-NULL emplty variable
+-Resource special variable that holds a resource
 */
+$name = 'Perez'; //String
+$age = 23; //Integer
+$WalMart = true; //Boolean
+$cash_on_hand = 2.75; //Float
+
+// var_dump($name); //
+
+echo $name . ' is ' . ' $age years old';
+
+echo "$name is $age years old";
+
+echo '5' + '5';
+$x = '5' + '5';
+var_dump($x);
+echo 10 - 5;
+echo 10 / 2;
+echo 10 % 3;
+
+// CONSTANTS
+define('HOST', 'localhost');
+define('DB_NAME', 'dev_db');
+
+echo HOST;
+
+
+
+
+
+
+
+
+
+
 ?>

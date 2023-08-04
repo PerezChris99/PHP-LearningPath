9fd20af737f157fed8a0deb7870020363274e9ae
PerezChris99 <kweeziperez712@gmail.com>
2023-08-04 13:14:05 +0300

    initial commit

---
+++ 01_output.php
@@ -0,0 +1,19 @@
+<?php
+    //echo 'Hello World', 10.5;
+    //print_r([1,2,3]);
+    //var_dump('Hello');
+    //var_export('Hello');
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1><?php echo 'Post one' ?></h1>
+</body>
+</html>
---
+++ 02_variables.php
@@ -0,0 +1,13 @@
+<?php
+/*----php data types---*/
+/* 
+string series of characters surrounded by quotes
+integer whole numbers
+float decimal numbers
+boolean true or false
+array special variable, which can hold more than one element or value
+object a class
+NULL emplty variable
+Resource special variable that holds a resource
+*/
+?>

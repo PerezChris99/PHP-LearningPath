c61b208ada52f356ebb8b5e9d4d300724eb4e3c4
PerezChris99 <kweeziperez712@gmail.com>
2023-08-08 12:35:13 +0300

    function to add numbers

--- 06_functions.php
+++ 06_functions.php
@@ -2,8 +2,16 @@
     function registerUser() {
         echo 'User registered';
     }
-    
+
     registerUser();
 
+    function sum($n1, $n2) {
+        return $n1 + $n2;
+    }
 
+    /*either*/
+    echo sum(5, 5);
+    /* or*/
+    $number = sum(5, 5);
+    echo $number;
 ?>

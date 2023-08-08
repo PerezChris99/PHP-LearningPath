5e6872b5d039d11ab8aa592c1d453808ba21a37c
PerezChris99 <kweeziperez712@gmail.com>
2023-08-08 16:08:55 +0300

    practive variables

---
+++ practice.php
@@ -0,0 +1,30 @@
+<?php
+    echo "Waguan";
+
+    //variables
+    $a = 10;
+    $b = 'this is a variable';
+
+    echo $a;
+    echo '<br>';
+    echo '<hr>';
+
+    echo $b;
+
+    // operators
+    
+
+
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>
